274e19da63e6f32b0604d42d6ad1259f17b1ae0d
Augustine LF <c.r.allen310@gmail.com>
2017-10-30 20:59:01 -0700

    nootes

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function logMessage() {
+  console.log ('hello world');
+};
+
+var name = "catherine";
+
+// es5 string interpolation below
+var message = "Hello there, " + name + "!";
+console.log(message);
+
+// es6 SI below
+var secondMessage = `hello thar, ${name}`
+console.log(secondMessage);
+
+
+
+var number = 14;
+console.log(number);
