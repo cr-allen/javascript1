b8009fd8efea65de886384e65bfc051b7a560c33
Augustine LF <c.r.allen310@gmail.com>
2017-11-06 18:26:27 -0800

    prompt stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,101 @@
-// var message = "yo";
-// console.log(typeof message);
-//
-// let fresh = true
-// console.log(fresh)
-//
-// const name = "ryan";
-// console.log(name);
-// if(fresh) {
-//   name = "rascal"
-//
-// }
-
-
-
-//   // var best = "star trek";
-// let best = "regulus";
-// const best = "auggie";
-// console.log(best);}
-
-
-
-  // var best = "auggie";
-// let best = "star trek";
-// const best = "regulus";
-// console.log(best);}
-//
-// {
-//   // var best = "regulus";
-// let best = "auggie";
-// const best = "star trek";}
-// console.log(best);
-
-// const is hella locked down
-
-// predefined objects
-
-// newDate
-// var rightNow = new Date( );
-// console.log(rightNow);
+// // var message = "yo";
+// // console.log(typeof message);
+// //
+// // let fresh = true
+// // console.log(fresh)
+// //
+// // const name = "ryan";
+// // console.log(name);
+// // if(fresh) {
+// //   name = "rascal"
+// //
+// // }
 //
 //
-// // math object
-// var randomNumber = Math.random ();
-// console.log(randomNumber);
-// // console.log(stringObj.split('0'));
 //
-// var name = 'janie';
+// //   // var best = "star trek";
+// // let best = "regulus";
+// // const best = "auggie";
+// // console.log(best);}
 //
-// // makes a string object like so:
-// var stringObj = new String ('john');
 //
-// // console.log(typeof name, typeof stringObj);
 //
-// console.log(stringObj.split('j').join('ough'));
-// console.log(name.length);
+//   // var best = "auggie";
+// // let best = "star trek";
+// // const best = "regulus";
+// // console.log(best);}
+// //
+// // {
+// //   // var best = "regulus";
+// // let best = "auggie";
+// // const best = "star trek";}
+// // console.log(best);
+//
+// // const is hella locked down
+//
+// // predefined objects
+//
+// // newDate
+// // var rightNow = new Date( );
+// // console.log(rightNow);
+// //
+// //
+// // // math object
+// // var randomNumber = Math.random ();
+// // console.log(randomNumber);
+// // // console.log(stringObj.split('0'));
+// //
+// // var name = 'janie';
+// //
+// // // makes a string object like so:
+// // var stringObj = new String ('john');
+// //
+// // // console.log(typeof name, typeof stringObj);
+// //
+// // console.log(stringObj.split('j').join('ough'));
+// // console.log(name.length);
+//
+// let bestNumber = 42;
+//
+// console.log(typeof bestNumber);
+//
+// let reallyBestNumber = new Number(42);
+// console.log(reallyBestNumber);
+//
+//
+// let bool = new Boolean(1);
+//
+// console.log(bool);
+//
+// //  ---------- key - value. can have more than one value.
+// let object = {name: 'sally'};
+// // console.log(object);
+//
+//
+// // alert('hey thar');
+// // var confirm = confirm('are u sure');
+// // console.log(confirm);
+// var userAnswer = prompt('what is your name');
+// console.log(userAnswer);
+//
+//
+//
+//
 
-let bestNumber = 42;
-
-console.log(typeof bestNumber);
-
-let reallyBestNumber = new Number(42);
-console.log(reallyBestNumber);
-
-
-let bool = new Boolean(1);
-
-console.log(bool);
-
-//  ---------- key - value. can have more than one value.
-let object = {name: 'sally'};
-// console.log(object);
-
-
-// alert('hey thar');
-// var confirm = confirm('are u sure');
-// console.log(confirm);
-var userAnswer = prompt('what is your name');
-console.log(userAnswer);
 
 
 
 
 
 
-
-
-
-
-
-
+let firstName = prompt('what\'s your first name?');
+console.log(firstName);
+let lastName = prompt('what\'s your last name?');
+alert(lastName);]
+let birthday = prompt('what\'s your birthday?', '12/12/2340');
+let isBirthdayCorrect = confirm('is ${birthday} your birthday?');
+// assuming user said yes
+alert(birthday);
 
 
 
