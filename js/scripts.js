d59520d105c09c26fa9f6e214417bd227fead99d
Augustine LF <c.r.allen310@gmail.com>
2017-12-11 17:37:18 -0800

    day10 stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,66 @@
-///copy over to day9
+//don't store passwords at all!!
+var loginButton = document.querySelector('button')
 
-var sayHello = function () {
-  alert('hey thar');
+loginButton.addEventListener('click', function(e) {
+  e.preventDefault();//mockup for submitting data and getting results. don't do irl
+  var username = document.forms[0].username.value;
+
+  localStorage.setItem("username", username); //can grab them in this scope
+});
+
+
+var editUser = function() {
+  var username = document.querySelector("update");
+  var containsClass=username.classList.contains("editMode"); //element might need to be li??
+  if (containsClass) {
+    username.innerText=editInput.value;
+  } else {
+    username.value=label.innerText;
+  }
+  listItem.classList.toggle("editMode");
+
 };
 
-setIverval(function () {sayHello();}, 2000) //basically, a method that takes two arguments.
+var clearUser = function() {
+  localStorage.clearItem("username", username);
+
+
+};
+
+var alert = function () {
+
+}
+
+var update = document.querySelector('#update'),
+clear = document.querySelector('#clear'),
+useralert = document.querySelector('#alert');
+
+var arr = [];
+arr.push(update,clear,useralert);
+
+//can also be done with querySelectorAll
+//args are what changes
+var attachListeners = function(element, evt) {
+  element.addEventListener(evt, function(e) {
+    switch (element) {
+      case update:
+        var itemToStore = document.forms.login.username.value;
+        localStorage.setItem('username',itemToStore);
+        break;
+      case clear:
+        localStorage.removeItem('username');
+        break;
+      case useralert:
+        let itemToAlert = localStorage.getItem('username');
+        console.log(alert('cheese'))
+        alert(itemToAlert);
+        break;
+    }
+  });
+};
+///for universal, write separate indivudal functions for each possibility
+//dynamic element and a dynamic event
+//switch is will stay the same
+for (let i = 0; i < arr.length; i++) {
+  attachListeners(arr[i], "click");
+};
